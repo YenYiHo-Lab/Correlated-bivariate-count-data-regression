6deeeb1f5f969e1da32c733d3d5bd0d31ff3a4df
ZichenMa-USC <50111936+ZichenMa-USC@users.noreply.github.com>
2019-05-05 13:22:19 -0400

    Rjags code for direct & indirect approach

--- NBinomJags.R
+++ NBinomJags.R
@@ -6,7 +6,7 @@ library(rjags)
 jags_data = list(N=n,Y=y,x=x)
 jags_inits = list(beta0=c(0,0),beta1=c(0,0),gamma0=c(0,0),gamma1=c(0,0),
 	tau=c(0,0))
-jags_model = jags.model("indirect.txt",data=jags_data,n.adapt=0,
+jags_model = jags.model("IndirectApproach_jags.txt",data=jags_data,n.adapt=0,
 	inits=jags_inits)
 update(jags_model,n.iter=2000)
 coda_sample = coda.samples(jags_model,variable.names=c('tau'),
@@ -16,7 +16,7 @@ coda_sample = coda.samples(jags_model,variable.names=c('tau'),
 
 jags_data = list(N=n,Y=y,x=x)
 jags_inits = list(beta=rep(0,4),gamma=rep(0,4),tau=rep(0,2))
-jags_model = jags.model("direct.txt",data=jags_data,n.adapt=0,
+jags_model = jags.model("DirectApproach_jags.txt",data=jags_data,n.adapt=0,
 	inits=jags_inits)
 update(jags_model,n.iter=2000)
 coda_sample = coda.samples(jags_model,variable.names=c('tau'),
